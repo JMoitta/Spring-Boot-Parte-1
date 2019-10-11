2bb9df762087a7ec5ec2b00c4beccdac7e4e5c26
João Moita Manrique <joaomoitamanrique@gmail.com.br>
2019-10-11 18:12:42 -0300

    Aula 02:05 (Uso da anotação @ResponseBody) e 06 (Usuando DTO)

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,21 +1,21 @@
 package br.com.alura.forum.controller;
 
+import br.com.alura.forum.dto.TopicoDto;
 import br.com.alura.forum.entity.Curso;
 import br.com.alura.forum.entity.Topico;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 public class TopicosController {
 
-    @RequestMapping("/toṕicos")
-    @ResponseBody
-    public List<Topico> lista() {
+    @RequestMapping("/topicos")
+    public List<TopicoDto> lista() {
         Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
-        return Arrays.asList(topico, topico, topico);
+        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
     }
 }
