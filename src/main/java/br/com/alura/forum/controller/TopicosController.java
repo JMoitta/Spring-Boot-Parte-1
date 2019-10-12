102dc02a8bf97406b45fa5fa2d139ed10d5f061b
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-10-12 13:36:23 -0300

    Aula 03:06 - Ultizando Repository

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,6 +3,8 @@ package br.com.alura.forum.controller;
 import br.com.alura.forum.dto.TopicoDto;
 import br.com.alura.forum.entity.Curso;
 import br.com.alura.forum.entity.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +15,12 @@ import java.util.List;
 @RestController
 public class TopicosController {
 
+    @Autowired
+    private TopicoRepository topicoRepository;
+
     @RequestMapping("/topicos")
     public List<TopicoDto> lista() {
-        Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
-        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+        List<Topico> topicos = topicoRepository.findAll();
+        return TopicoDto.converter(topicos);
     }
 }
