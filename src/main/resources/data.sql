3f2f43622147eb90d50c6a1263ea27825a2e7177
João Moita Manrique <joaomoitamanrique@gmail.com.br>
2019-10-16 13:25:09 -0300

    Aula 03:08 - Autenticando o usuário

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$ca8BI6KvgFz8TL0UJNPnVu5T5tpeDxvupFDczLf6/2zjIN46FrBD6');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
